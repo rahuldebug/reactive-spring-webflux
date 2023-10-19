0ff28585c1edc8ef07cb5a173950d6eee7191c57
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-10-19 13:42:34 +0530

    flux immutability

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -15,11 +15,17 @@ public class FluxAndMonoGeneratorService {
     public Flux<String> nameFluxMap() {
         return Flux.fromIterable(List.of("rahul", "manas", "pillu", "papa")).map(String::toUpperCase);
     }
+    public Flux<String> nameFluxImmutability() {
+        var immutableFlux=Flux.fromIterable(List.of("rahul", "manas", "pillu", "papa"));
+        immutableFlux.map(String::toUpperCase);
+        return immutableFlux;
+    }
 
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
         fluxAndMonoGeneratorService.nameFlux().subscribe(n -> System.out.println(n));
         fluxAndMonoGeneratorService.nameMono().subscribe(n -> System.out.println(n));
         fluxAndMonoGeneratorService.nameFluxMap().subscribe(n -> System.out.println(n));
+        fluxAndMonoGeneratorService.nameFluxImmutability().subscribe(n -> System.out.println(n));
     }
 }
