de4a779cacec05a041edd8389e43dec95d519c6d
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-10-19 16:15:55 +0530

    flux with multiple operations

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -9,16 +9,26 @@ public class FluxAndMonoGeneratorService {
     public Flux<String> nameFlux() {
         return Flux.fromIterable(List.of("rahul", "manas", "pillu", "papa"));
     }
-    public Mono<String> nameMono(){
+
+    public Mono<String> nameMono() {
         return Mono.just("mono");
     }
+
     public Flux<String> nameFluxMap() {
-        return Flux.fromIterable(List.of("rahul", "manas", "pillu", "papa")).map(String::toUpperCase);
+        return Flux.fromIterable(List.of("rahul", "manas", "pillu", "papa"))
+                .map(String::toUpperCase);
     }
+
     public Flux<String> nameFluxImmutability() {
-        var immutableFlux=Flux.fromIterable(List.of("rahul", "manas", "pillu", "papa"));
+        var immutableFlux = Flux.fromIterable(List.of("rahul", "manas", "pillu", "papa"));
         immutableFlux.map(String::toUpperCase);
         return immutableFlux;
+    }
+
+    public Flux<String> multipleOperations(int len) {
+        return Flux.fromIterable(List.of("rahul", "manas", "pillu", "papa")).
+                map(String::toUpperCase).
+                filter(n -> n.length() > len);
     }
 
     public static void main(String[] args) {
@@ -27,5 +37,6 @@ public class FluxAndMonoGeneratorService {
         fluxAndMonoGeneratorService.nameMono().subscribe(n -> System.out.println(n));
         fluxAndMonoGeneratorService.nameFluxMap().subscribe(n -> System.out.println(n));
         fluxAndMonoGeneratorService.nameFluxImmutability().subscribe(n -> System.out.println(n));
+        fluxAndMonoGeneratorService.multipleOperations(3).subscribe(n-> System.out.println(n));
     }
 }
