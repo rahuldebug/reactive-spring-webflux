c366c1b114ca6215daa2cf6ff2131ce6228a25db
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-10-19 12:52:25 +0530

    map operation

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -12,10 +12,14 @@ public class FluxAndMonoGeneratorService {
     public Mono<String> nameMono(){
         return Mono.just("mono");
     }
+    public Flux<String> nameFluxMap() {
+        return Flux.fromIterable(List.of("rahul", "manas", "pillu", "papa")).map(String::toUpperCase);
+    }
 
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
         fluxAndMonoGeneratorService.nameFlux().subscribe(n -> System.out.println(n));
         fluxAndMonoGeneratorService.nameMono().subscribe(n -> System.out.println(n));
+        fluxAndMonoGeneratorService.nameFluxMap().subscribe(n -> System.out.println(n));
     }
 }
